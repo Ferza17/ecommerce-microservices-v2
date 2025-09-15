b2f8c12eae1ad38b69e5546f741bd0a0508edcb7
ferza <feryreza85@gmail.com>
2025-09-15 10:50:57 +0700

    add mongodb stream connector

--- entrypoint/mongo/mongo-init.js
+++ entrypoint/mongo/mongo-init.js
@@ -1,8 +1,10 @@
-// INIT DATABASE NOTIFICATION
+
+// Create databases & collections
+print("Creating database notifications & collections...");
 db = db.getSiblingDB('notification');
 db.createCollection('notification_templates');
 
-// INIT DATABASE COMMERCE
+print("Creating database commerce & collections...");
 db = db.getSiblingDB('commerce');
 db.createCollection('carts');
 db.createCollection('wishlists');
