43ba89a4a95068528a2129567611757549d44582
ferza <feryreza85@gmail.com>
2025-10-16 20:03:25 +0700

    modify commerce svc

--- services/commerce-service/src/main/java/com/ferza17/ecommercemicroservicesv2/commerceservice/config/OpenTelemetryConfig.java
+++ services/commerce-service/src/main/java/com/ferza17/ecommercemicroservicesv2/commerceservice/config/OpenTelemetryConfig.java
@@ -1,35 +1,4 @@
 package com.ferza17.ecommercemicroservicesv2.commerceservice.config;
 
-import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import io.opentelemetry.sdk.resources.Resource;
-
-import static io.opentelemetry.semconv.ServiceAttributes.SERVICE_NAME;
-
-
-@Configuration
 public class OpenTelemetryConfig {
-    @Value("${spring.application.name:commerce-service}")
-    private String serviceName;
-
-    @Bean
-    public io.opentelemetry.sdk.OpenTelemetrySdk setupOpenTelemetry() {
-        return OpenTelemetrySdk
-                .builder()
-                .setTracerProvider(SdkTracerProvider
-                        .builder()
-                        .addSpanProcessor(BatchSpanProcessor
-                                .builder(JaegerGrpcSpanExporter.builder().setEndpoint("http://localhost:4317").build())
-                                .build()
-                        )
-                        .setResource(Resource.getDefault().merge(Resource.create(io.opentelemetry.api.common.Attributes.of(SERVICE_NAME, this.serviceName))))
-                        .build()
-                )
-                .build();
-    }
 }
